1d0098f7eb881fd1426d96f8e31269566ebda3a0
koola123 <82808273+koola123@users.noreply.github.com>
2021-07-27 20:06:25 +0200

    deactivated JWT token

--- index.js
+++ index.js
@@ -34,14 +34,12 @@ app.get('/', (req, res) => {
 });
 
 // GETS A LIST OF ALL MOVIES
-app.get('/movies', passport.authenticate('jwt', {
-  session: false
-}), (req, res) => {
+app.get('/movies', function (req, res) => {
   Movies.find()
-    .then((movies) => {
+    .then(function(movies) {
       res.status(201).json(movies);
     })
-    .catch((err) => {
+    .catch(function(err) {
       console.error(err);
       res.status(500).send('Error: ' + err);
     });
