09cfff78088fe2ade9b44c819b2752e4616df651
koola123 <82808273+koola123@users.noreply.github.com>
2021-08-10 20:31:00 +0200

    added new  endpoint for single user

--- index.js
+++ index.js
@@ -250,6 +250,19 @@ app.delete('/users/:Username', passport.authenticate('jwt', {session: false}), (
     });
 });
 
+// GETS A SINGLE USER
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Users.findOne({
+      Username: req.params.Username })
+      .then((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: '' + err);
+      });
+  });
+
 // GETS A LIST OF ALL USERS
 app.get('/users/', passport.authenticate('jwt', {session: false}), (req, res) => {
   Users.find()
