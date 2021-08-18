b0a28cc731018489c831b499a56e6cb3f6d7fe03
koola123 <82808273+koola123@users.noreply.github.com>
2021-08-18 17:09:01 +0200

    added users profile info as a new endpoint

--- index.js
+++ index.js
@@ -263,6 +263,43 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
       });
   });
 
+
+  // UPDATE A USER'S PROFILE INFO
+app.put('/users/:Username', [
+  check('Username', 'Username required').isLength({ min: 5 }),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], (req, res) => {
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
+
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $set:
+    {
+      Username: req.body.Username,
+      Password: hashedPassword,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+});
+
+
 // GETS A LIST OF ALL USERS
 app.get('/users/', passport.authenticate('jwt', {session: false}), (req, res) => {
   Users.find()
